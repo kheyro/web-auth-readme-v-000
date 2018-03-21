d21e855ab0c441309e5fa25f944542e71dcbffe5
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-21 10:12:21 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,33 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user
+  skip_before_action :authenticate_user, only: :create
+
+  def create
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+      req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['code'] = params[:code]
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
+  end
+
+  private
+
+    def authenticate_user
+      client_id = ENV['FOURSQUARE_CLIENT_ID']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+      redirect_to foursquare_url unless logged_in?
+    end
+
+    def logged_in?
+      !!session[:token]
+    end
 end
